28f0cc128ef0793de9962459dfd206886be859fc
ulucam <82106925+ulucam@users.noreply.github.com>
2026-07-31 16:58:21 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,12 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from typing import Generator
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-# SQLite kullan (geçici olarak)
 DATABASE_URL = "sqlite:///muys.db"
 
 engine = create_engine(
