9ca7bad8ed3fc22ad32f18e5afc43684195f807b
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-02 01:54:46 +0300

    Update receteler.py

--- app/routes/receteler.py
+++ app/routes/receteler.py
@@ -1,1 +1,18 @@
+from fastapi import APIRouter, Request, Depends
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
 
+from app.security import yetki_kontrol
+from app.database import get_db
+from app.roles import RECETE
+
+router = APIRouter(
+    prefix="/recete",
+    tags=["Reçeteler"]
+)
+
+
+templates = Jinja2Templates(
+    directory="app/templates"
+)
