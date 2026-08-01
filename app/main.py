5be9f130f7ea851325535eb717b1a5e3d682b28f
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-02 01:55:09 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,29 +1,40 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from starlette.middleware.sessions import SessionMiddleware
 
 from app.database import Base, engine
 
 from app.routes import auth
 from app.routes import dashboard
+from app.routes import kullanicilar
 from app.routes import musteriler
-from app.routes import urunler
+
+
 
 
 Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(
-    title="MÜYS"
+    title="MÜYS - Üretim Yönetim Sistemi"
+)
+
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
 )
 
 
 app.add_middleware(
     SessionMiddleware,
-    secret_key="gizli"
+    secret_key="muys-secret-key-2026"
 )
 
 
+# ROUTES
 app.include_router(auth.router)
 app.include_router(dashboard.router)
+app.include_router(kullanicilar.router)
 app.include_router(musteriler.router)
-app.include_router(urunler.router)
