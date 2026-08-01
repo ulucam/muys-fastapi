1417a1bdf2dd53165a9a001986e0cd78255f2e2c
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-01 13:50:27 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,24 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from typing import Generator
-
-DATABASE_URL = "sqlite:///muys.db"
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.config import Config
 
 engine = create_engine(
-    DATABASE_URL,
-    echo=False,
-    connect_args={"check_same_thread": False}
+    Config.DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in Config.DATABASE_URL else {}
 )
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-def get_db() -> Generator:
+def get_db():
     db = SessionLocal()
     try:
         yield db
