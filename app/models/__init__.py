6c9cd6dab12dc6b2e5b5a93aa223144d00ee0552
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-01 16:19:11 +0300

    Import models in app/models/__init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,8 @@
-
+from .user import User
+from .musteri import Musteri
+from .urun import Urun
+from .siparis import Siparis
+from .recete import Recete
+from .stok import Stok
+from .uretim import Uretim
+from .sevkiyat import Sevkiyat
