db7ea05aa9af1b5f899d421239e90ae81adaf4e8
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-01 16:20:41 +0300

    Update auth_service.py

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,1 +1,17 @@
+from passlib.context import CryptContext
 
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
+
+
+def sifre_hashle(sifre: str):
+    return pwd_context.hash(sifre)
+
+
+def sifre_dogrula(duz_sifre: str, hashli_sifre: str):
+    return pwd_context.verify(
+        duz_sifre,
+        hashli_sifre
+    )
