eee03a5389ba5f6ebed8a4635d02eb63af1de7a7
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-03 10:55:10 +0300

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,1 +1,5 @@
 from . import auth
+from app.routes.dashboard import dashboard_bp
+from app.routes.musteriler import musteri_bp
+from app.routes.urunler import urun_bp
+from app.routes.ayarlar import ayarlar_bp
