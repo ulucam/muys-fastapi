f4c8af3c2b0c9490694587232f7d017e2fd776df
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-05 11:11:00 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,6 +14,7 @@ from app.models.personel import Personel
 from app.models.istasyon import Istasyon
 from app.models.makine import Makine
 from app.models.personel_makine import PersonelMakine
+from app.models.puantaj import Puantaj
 from app.models.urun_sinifi import UrunSinifi
 from app.models.urun_sinif_operasyon import UrunSinifOperasyon
 
