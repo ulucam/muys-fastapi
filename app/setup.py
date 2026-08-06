b1e14794e3c7c3dc0007b8d4834d24e530c942db
ulucam <82106925+ulucam@users.noreply.github.com>
2026-08-06 07:20:43 +0300

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -2,11 +2,17 @@ from sqlalchemy.orm import Session
 
 from app.models.user import User
 from app.password import sifre_olustur
+from app.models.stok_urun_turu import StokUrunTuru
 
 
 def setup_database(db: Session):
 
     try:
+
+        for ad, uretilen in [("Hammadde", False), ("Sarf Malzeme", False), ("Üretim", True), ("Ticari Mamül", False)]:
+            if not db.query(StokUrunTuru).filter(StokUrunTuru.adi == ad).first():
+                db.add(StokUrunTuru(adi=ad, uretilen=uretilen, aktif=True))
+        db.commit()
 
         admin = (
             db.query(User)
